d0107a58bcb4bd66a6b2eb9a6be1d15031dff71f
Samuel <samuel@dreipol.ch>
2014-03-19 14:49:40 +0100

    initial

---
+++ blinq/src/main/java/ch/dreipol/android/blinq/ApiManager.java
@@ -0,0 +1,48 @@
+package ch.dreipol.android.blinq;
+
+import java.util.HashMap;
+import java.util.List;
+
+import retrofit.RestAdapter;
+import retrofit.http.Body;
+import retrofit.http.POST;
+import rx.Observable;
+import rx.Observer;
+import rx.Subscription;
+import rx.schedulers.Schedulers;
+import rx.subscriptions.Subscriptions;
+
+/**
+ * Created by melbic on 14/03/14.
+ */
+public class ApiManager {
+
+    private static HashMap map;
+
+    private interface ApiManagerService {
+        @POST("/swarm")
+        List<Profile> getSwarm(@Body HashMap body );
+    }
+
+    private static final RestAdapter restAdapter = new RestAdapter.Builder()
+            .setServer("http://api.openweathermap.org/data/2.5")
+            .build();
+    private static final ApiManagerService apiManager = restAdapter.create(ApiManagerService.class);
+
+    public static Observable<Profile> getSwarm(final String city) {
+        return Observable.from(new Observable.OnSubscribeFunc<Profile>() {
+            @Override
+            public Subscription onSubscribe(Observer<? super Profile> observer) {
+                try {
+                    observer.onNext(apiManager.getSwarm(map));
+                    observer.onCompleted();
+                } catch (Exception e) {
+                    observer.onError(e);
+                }
+
+                return Subscriptions.empty();
+            }
+        }).subscribeOn(Schedulers.threadPoolForIO());
+    }
+
+}
---
+++ blinq/src/main/java/ch/dreipol/android/blinq/Mutual_data.java
@@ -0,0 +1,48 @@
+
+package ch.dreipol.android.blinq;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Mutual_data {
+
+    private List<Object> schools = new ArrayList<Object>();
+    private List<Object> likes = new ArrayList<Object>();
+    private List<Object> places = new ArrayList<Object>();
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    public List<Object> getSchools() {
+        return schools;
+    }
+
+    public void setSchools(List<Object> schools) {
+        this.schools = schools;
+    }
+
+    public List<Object> getLikes() {
+        return likes;
+    }
+
+    public void setLikes(List<Object> likes) {
+        this.likes = likes;
+    }
+
+    public List<Object> getPlaces() {
+        return places;
+    }
+
+    public void setPlaces(List<Object> places) {
+        this.places = places;
+    }
+
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
