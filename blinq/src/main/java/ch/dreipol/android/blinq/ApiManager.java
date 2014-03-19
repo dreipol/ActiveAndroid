fa5ef42e26fa0f3d383a46b3768cfe21a6b080f5
Samuel <samuel@dreipol.ch>
2014-03-19 15:33:17 +0100

    Added ConnectionSignature

--- blinq/src/main/java/ch/dreipol/android/blinq/ApiManager.java
+++ blinq/src/main/java/ch/dreipol/android/blinq/ApiManager.java
@@ -1,48 +1,75 @@
 package ch.dreipol.android.blinq;
 
+import android.util.Log;
+
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import retrofit.RestAdapter;
 import retrofit.http.Body;
 import retrofit.http.POST;
 import rx.Observable;
-import rx.Observer;
-import rx.Subscription;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
+import rx.functions.Func1;
 import rx.schedulers.Schedulers;
-import rx.subscriptions.Subscriptions;
 
 /**
  * Created by melbic on 14/03/14.
  */
 public class ApiManager {
 
-    private static HashMap map;
-
+    private static Map map;
+    static {
+        Map<String, String> aMap = new HashMap<String,String>();
+        map.put()
+        map = Collections.unmodifiableMap(aMap);
+    }
     private interface ApiManagerService {
         @POST("/swarm")
-        List<Profile> getSwarm(@Body HashMap body );
+        Observable<List<Profile>> getSwarm(@Body Map body);
     }
 
     private static final RestAdapter restAdapter = new RestAdapter.Builder()
-            .setServer("http://api.openweathermap.org/data/2.5")
+            .setEndpoint("http://api.openweathermap.org/data/2.5")
             .build();
     private static final ApiManagerService apiManager = restAdapter.create(ApiManagerService.class);
 
-    public static Observable<Profile> getSwarm(final String city) {
-        return Observable.from(new Observable.OnSubscribeFunc<Profile>() {
-            @Override
-            public Subscription onSubscribe(Observer<? super Profile> observer) {
-                try {
-                    observer.onNext(apiManager.getSwarm(map));
-                    observer.onCompleted();
-                } catch (Exception e) {
-                    observer.onError(e);
-                }
+    public ApiManager() {
+    }
 
-                return Subscriptions.empty();
-            }
-        }).subscribeOn(Schedulers.threadPoolForIO());
+    public Observable<Profile> getSwarm() {
+        apiManager.getSwarm(map)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .flatMap(new Func1<List<Profile>, Observable<Profile>>() {
+                    @Override
+                    public Observable<Profile> call(List<Profile> profiles) {
+                        return Observable.from(profiles);
+                    }
+                })
+                .subscribe(new Action1<Profile>() {
+                    @Override
+                    public void call(Profile profile) {
+                        Log.d("ch.blinq", profile.toString());
+                    }
+                });
+
+//        return Observable.from(new Observable.OnSubscribeFunc<Profile>() {
+//            @Override
+//            public Subscription onSubscribe(Observer<? super Profile> observer) {
+//                try {
+//                    observer.onNext();
+//                    observer.onCompleted();
+//                } catch (Exception e) {
+//                    observer.onError(e);
+//                }
+//
+//                return Subscriptions.empty();
+//            }
+//        }).subscribeOn(Schedulers.threadPoolForIO());
     }
 
 }
