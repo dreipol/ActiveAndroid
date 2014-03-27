1b646b0677c4ce9c764c9ee501327b006cfb7b5e
phil <philipp.laeubli@dreipol.ch>
2014-03-27 16:59:38 +0100

    added nineslices

--- blinq/src/main/java/ch/dreipol/android/blinq/ui/buttons/SettingsButton.java
+++ blinq/src/main/java/ch/dreipol/android/blinq/ui/buttons/SettingsButton.java
@@ -3,6 +3,7 @@ package ch.dreipol.android.blinq.ui.buttons;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.util.AttributeSet;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -16,9 +17,6 @@ public class SettingsButton extends RelativeLayout {
     public SettingsButton(Context context, AttributeSet attrs) {
         super(context, attrs);
 
-
-
-
         init(attrs);
     }
 
@@ -26,16 +24,24 @@ public class SettingsButton extends RelativeLayout {
         TypedArray a = getContext().obtainStyledAttributes(attrs, R.styleable.SettingsButton);
 
         ImageView leftImageView = new ImageView(getContext());
-        ImageView rightImageView = new ImageView(getContext());
+
+        LayoutParams p = new LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        p.addRule(ALIGN_PARENT_LEFT, CENTER_IN_PARENT);
+
+        LayoutParams pT = new LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        pT.addRule(CENTER_IN_PARENT, CENTER_IN_PARENT);
+
+
         TextView textView = new TextView(getContext());
 
-        addView(leftImageView);
-        addView(textView);
-        addView(rightImageView);
 
         textView.setText(a.getString(R.styleable.SettingsButton_buttonText));
+        textView.setTextColor(a.getColor(R.styleable.SettingsButton_buttonTextColor, 1));
         leftImageView.setImageDrawable(a.getDrawable(R.styleable.SettingsButton_leftImage));
-        rightImageView.setImageDrawable(a.getDrawable(R.styleable.SettingsButton_rightImage));
+
+
+        addView(leftImageView,p);
+        addView(textView, pT);
 
         a.recycle();
 
