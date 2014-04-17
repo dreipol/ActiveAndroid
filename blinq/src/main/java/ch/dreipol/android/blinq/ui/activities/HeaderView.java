8dfe580ae2adead75eff2c3ae17ba3a62e45fe43
phil <philipp.laeubli@dreipol.ch>
2014-04-17 18:30:11 +0200

    added clicklisteners

--- blinq/src/main/java/ch/dreipol/android/blinq/ui/activities/HeaderView.java
+++ blinq/src/main/java/ch/dreipol/android/blinq/ui/activities/HeaderView.java
@@ -2,6 +2,7 @@ package ch.dreipol.android.blinq.ui.activities;
 
 import android.content.Context;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.RelativeLayout;
 
 import ch.dreipol.android.blinq.R;
@@ -11,10 +12,26 @@ import ch.dreipol.android.blinq.services.AppService;
  * Created by phil on 17.04.14.
  */
 public class HeaderView extends RelativeLayout {
+
+
     public HeaderView(Context context) {
         super(context);
         LayoutInflater inflater = AppService.getInstance().getRuntimeService().getLayoutInflator(context);
         inflater.inflate(R.layout.header_layout, this, true);
+
+        findViewById(R.id.settings_button).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        findViewById(R.id.match_button).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
 
     }
 }
